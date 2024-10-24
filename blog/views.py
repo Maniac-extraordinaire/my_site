2b71a24e2552ca715d9edbf43f04336580985fc2
Himanshu Singh <himanshusatna444@gmail.com>
2024-10-24 08:33:45 +0530

    Implemented models

--- blog/views.py
+++ blog/views.py
@@ -1,85 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from datetime import date
+from .models import Post
 
-all_posts=[
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Himanshu Singh",
-        "date": date(2024, 7, 10),
-        "title": "Mountain Hiking",
-        "excerpt" : " Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus inventoreiste cupiditate quos sapiente perspiciatis.",
-        "content" : """ Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Natus inventoreiste cupiditate quos sapiente perspiciatis.Lorem ipsum dolor sit amet
-                        consectetur, adipisicing elit. Laboriosam ratione fugiat itaque expedita
-                        reiciendis corporis cum cumque, tenetur aut praesentium totam quaerat
-                        excepturi, recusandae aspernatur delectus ex pariatur, veritatis alias?"""
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "work.png",
-        "author": "Himanshu Singh",
-        "date": date(2024, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Himanshu Singh",
-        "date": date(2024, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-def get_date(post):
-    return post['date']
 
 # Create your views here.
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "index_page_posts": latest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)   #So slug on the right side is this argument,slug on the left side is the name of that property of that attribute in our model.
     return render(request, "blog/post-detail.html",{
-        "post": identified_post
+        "post": identified_post,
+        "post_tags" : identified_post.tags.all()
     })
