298e3d877f1b8ce3b1fa4ada1890d99f9c0d31ba
Himanshu Singh <himanshusatna444@gmail.com>
2024-11-15 21:30:24 +0530

    "Added Comment model and form, updated post-detail.html template, modified SinglePostView to handle comments, and made corresponding changes to models.py, post-detail.css, and views.py."

--- blog/models.py
+++ blog/models.py
@@ -28,8 +28,13 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators = [MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True,  related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True,  related_name="posts")   #every post had one author but one author could have many posts,we added the author field to the post. OneToMany Relation
     tags = models.ManyToManyField(Tag)
     
     
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     
